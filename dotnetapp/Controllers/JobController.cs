8dd8dd847b620c751183cf60f392b0d8acd93656
roop9390 <kumargubbala94@gmail.com>
2023-07-12 10:45:09 +0000

    Roopkumar

---
+++ dotnetapp/Controllers/JobController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Models;
+
+namespace dotnetapp.Controllers
+{
+    [ApiController]
+   
+    public class JobController : ControllerBase
+    {
+        private BusinessLayer bussiness_layer = new BusinessLayer();
+        
+        //Get the all user posted jobs data
+        [HttpGet("admin/getalljobs")]
+        public IActionResult GetJob()
+        {
+            List<JobModel> result = bussiness_layer.getJob();
+            return Ok(result);
+        }
+
+        //Add new a job data
+        [HttpPost("admin/addJob")]
+        public IActionResult addJob([FromBody] JobModel Data)
+        {
+            string result = bussiness_layer.addJob(Data);
+            return Ok(result);
+        }
+
+        //Edit job data by jobId
+        [HttpPut("admin/editJob/{jobId}")]
+        public IActionResult editJob(string jobId,[FromBody] JobModel Data)
+        {
+            string result = bussiness_layer.editJob(jobId,Data);
+            return Ok(result);
+        }
+
+        //Delete job by jobId
+        [HttpDelete("admin/deleteJob/{jobId}")]
+        public IActionResult deletejob(string jobId)
+        {
+            string result = bussiness_layer.deletejob(jobId);
+            return Ok(result);
+        }
+
+        //get the list of jobs applied by jobseeker by id
+        [HttpGet("jobseeker/getAppliedjobs/{personId}")]
+        public IActionResult appliedjobs(string personId)
+        {
+            List<personModel> result= bussiness_layer.appliedjobs(personId);
+            return Ok(result);
+        }
+    }
+}
---
+++ reactapp/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import PrivateRoute from './components/auth/PrivateRoutes.jsx';
+import AdminBaseComponent from './components/Admin/AdminBaseComponent.jsx';
+import CustomerBaseComponent from './components/Customer/CustomerBaseComponent.jsx';
+import JobseekerBaseComponent from './components/Jobseeker/JobseekerBaseComponent.jsx';
+
+function App() {
+  return (
+
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/Admin/*' element={<PrivateRoute element={<AdminBaseComponent />} authRole="Admin" allowedRoles={['Admin']} />} />
+          <Route path="/customer/*" element={<PrivateRoute element={<CustomerBaseComponent />} authRole="User" allowedRoles={['User']} />} />
+          <Route path='/jobseeker/*' element={<PrivateRoute element={<JobseekerBaseComponent />} authRole="Jobseeker" allowedRoles={['Jobseeker']}  />} />
+          
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/Signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+
+  );
+}
+export default App;
