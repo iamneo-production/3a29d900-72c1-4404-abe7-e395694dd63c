0212a4cc59bdab64c00bc9d51a3d3cf4fcb5b41e
vinesh-ravella <ravellavinesh@gmail.com>
2023-07-15 14:22:31 +0000

    Vinesh

--- reactapp/src/axios.jsx
+++ reactapp/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://8080-ebdbcdadadfaffccecdfccfeafefcdfdfda.project.examly.io/"
+    baseURL: "https://8080-cfcaacebbcefaffccecdfccfeafefcdfdfda.project.examly.io/"
 }); 
 
 export default API;
