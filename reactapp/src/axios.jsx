8c14a896372addda0d2b7c38d5ce6c6ada389a09
deepthiinduri <deepthiinduri@gmail.com>
2023-07-15 14:22:29 +0000

    Induri Deepthi

--- reactapp/src/axios.jsx
+++ reactapp/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://8080-ebdbcdadadfaffccecdfccfeafefcdfdfda.project.examly.io/"
+    baseURL: "https://8080-ddfbcadcffaffccecdfccfeafefcdfdfda.project.examly.io/"
 }); 
 
 export default API;
